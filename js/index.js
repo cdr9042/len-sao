e2c8d36823f9aa0aa8173f57d60087c7bb5ce447
trong <cdr9042@gmail.com>
2018-01-19 17:40:51 +0700

    no message

--- js/index.js
+++ js/index.js
@@ -6,21 +6,24 @@ function init() { //chèn ảnh
     img = '<div><img src=' + '../assets/img/actor/actor-' + i.toString() + '.jpg></div>';
     html = html + img;
   }
-  $('#actor-slide').append(html);
+  $('#actor-slide').append(html); //thêm ảnh cho slide
+  
+  html = '<button type="button" class="btn bold">Đăng ký ngay</button>';
+  $('.add-buy-button').append(html); // thêm nút đăng ký cho những thẻ có class này
 }
 
 // var owl = $('.owl-carousel');
 // owl.owlCarousel();
 
 $(document).ready(function(){
-  // init();
+  init();
   $(".owl-carousel").owlCarousel({
-    items:4,
+    items:5,
     loop:true,
     margin:10,
     autoplay:true,
-    autoplayTimeout:7000,
-    autoplayHoverPause:true,
+    autoplayTimeout:2500,
+    autoplayHoverPause:false,
 });
 });
 
